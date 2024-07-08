24ae3d8c264cc57970c1ab3b7cfa2e8ced7c4fb2
Łukasz <ntnrmedia@gmail.com>
2024-07-08 19:13:44 +0200

    Header v2

--- html/headerfooter.js
+++ html/headerfooter.js
@@ -1,29 +1,26 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <header class="fixed left-0 h-full text-white flex flex-row justify-center">
-                <div class="flex flex-col align-middle justify-center gap-5 bg-gray-800 h-full w-16">
-                    <div class="nav-bar-icons"><a href=""><i class="material-symbols-outlined">home</i></a></div>
-                    <div class="nav-bar-icons"><a href=""><i class="material-symbols-outlined">apartment</i></a></div>
-                    <div class="nav-bar-icons"><a href=""><i class="material-symbols-outlined">photo_library</i></a></div>
-                    <div class="nav-bar-icons"><a href=""><i class="material-symbols-outlined">payments</i></a></div>
-                    <div class="nav-bar-icons"><a href=""><i class="material-symbols-outlined">chat</i></a></div>
-                </div>
-                
-                <div class="w-48 bg-gray-700 bg-opacity-95 flex flex-col justify-center gap-5 h-ful">
-                    <div class="nav-bar-items"><a href="">Strona Główna</a></div>
-                    <div class="nav-bar-items">Apartamenty <i id="drop-down-icon" class="material-symbols-outlined">keyboard_arrow_down</i></div>
-                    <div>
-                        <p class="text-xs">Apartamenty dwupoziomowe<i id="drop-down-icon-deep" class="material-symbols-outlined">keyboard_arrow_down</i></p>
-                        <a href="a5.html" class="text-xs">Apartament Dwupokojowy</a>
-                        <a href="domek.html" class="text-xs">Domek Drewniany</a>
-                    </div>
-                    <div class="nav-bar-items"><a href="galeria.html">Galeria</a></div>
-                    <div class="nav-bar-items"><a href="cennik.html">Cennik</a></div>
-                    <div class="nav-bar-items"><a href="">Kontakt</a></div>
-                </div>
-                
-            </header>
+        <header id="header" class="fixed left-0 h-full text-white flex flex-row justify-center">
+            <div class="flex flex-col align-middle justify-center gap-5 bg-gray-800 h-full w-16">
+                <div class="nav-bar-icons"><a href="index.html"><i class="material-symbols-outlined">home</i></a><div class="sidebar-child">
+                    <p>Strona Główna</p>
+                </div></div>
+                <div class="nav-bar-icons"><a href=""><i class="material-symbols-outlined">apartment</i></a><div class="sidebar-child">
+                    <p>Apartamenty</p>
+                </div></div>
+                <div class="nav-bar-icons"><a href="galeria.html"><i class="material-symbols-outlined">photo_library</i></a><div class="sidebar-child">
+                    <p>Galeria Zdjęć</p>
+                </div></div>
+                <div class="nav-bar-icons"><a href="cennik.html"><i class="material-symbols-outlined">payments</i></a><div class="sidebar-child">
+                    <p>Cennik</p>
+                </div></div>
+                <div class="nav-bar-icons"><a href="kontakt.html"><i class="material-symbols-outlined">chat</i></a><div class="sidebar-child">
+                    <p>Kontakt</p>
+                </div></div>
+            </div>
+            
+        </header>
         `
     }
 }
