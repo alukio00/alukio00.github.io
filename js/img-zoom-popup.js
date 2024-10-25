0ef957a60442724307606fc063adc22e9a2cae10
Łukasz <ntnrmedia@gmail.com>
2024-10-25 21:49:06 +0200

    sortowanie zdjęć

--- js/img-zoom-popup.js
+++ js/img-zoom-popup.js
@@ -3,18 +3,18 @@ class PopupImageZoom extends HTMLElement {
         this.innerHTML = `
         <div id="popup-div" class="hidden fixed left-0 w-full h-full flex justify-center items-center flex-row z-30 bg-black bg-opacity-60">
           
-            <button onclick="zoomLeft()" type="button" class="border-2 border-gray-800 hover:bg-gray-900 absolute left-2  flex cursor-pointer bg-slate-200 rounded-full text-gray-800 justify-center items-center"><i class="material-symbols-outlined">chevron_left</i></button>
+            <button onclick="zoomLeft()" type="button" class="border-2 border-gray-800 hover:bg-slate-400 duration-200 absolute left-2  flex cursor-pointer bg-slate-200 rounded-full text-gray-800 justify-center items-center"><i class="material-symbols-outlined">chevron_left</i></button>
             
 
 
             <div id="zoom-div" class="flex items-center justify-center"></div>
 
           
-            <button onclick="zoomRight()" type="button" class="border-2 border-gray-800 hover:bg-gray-900 absolute right-2  cursor-pointer flex bg-slate-200 rounded-full text-gray-800 justify-center items-center"><i class="material-symbols-outlined">chevron_right</i></button>
+            <button onclick="zoomRight()" type="button" class="border-2 border-gray-800 hover:bg-slate-400 duration-200 absolute right-2  cursor-pointer flex bg-slate-200 rounded-full text-gray-800 justify-center items-center"><i class="material-symbols-outlined">chevron_right</i></button>
            
 
             <div id="popup-btn" class="flex z-40 fixed top-2 right-2">
-                <button class="text-gray-800 material-symbols-outlined rounded-full border-2 border-gray-800 bg-slate-200" type="button" onclick="closeZoom()">close</button>
+                <button class="text-gray-800 material-symbols-outlined rounded-full border-2 hover:bg-slate-400 duration-200 border-gray-800 bg-slate-200" type="button" onclick="closeZoom()">close</button>
             </div>
         </div>
         `;
