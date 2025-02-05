da64624a8703d777874513b1c7a0ce674ef26d7f
Łukasz <ntnrmedia@gmail.com>
2025-02-05 20:13:29 +0100

    seo fix

--- js/opinie.js
+++ js/opinie.js
@@ -48,7 +48,7 @@ class ReviewsWidget extends HTMLElement {
 
         this.innerHTML = `
             <section class="flex flex-col gap-14 justify-center">
-                <h2 class="text-center font-medium text-4xl">Opinie</h2>
+                <h2 class="text-center font-medium text-4xl">Cichy Zakątek Opinie</h2>
                 <div class="w-full max-w-10xl mx-auto justify-center flex-col items-center flex gap-8">
                     <div class="flex gap-8 justify-center max-lg:flex-col">
                         ${htmlReviews.slice(0, 2).join("")}
