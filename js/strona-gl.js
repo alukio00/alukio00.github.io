5eac83d9879761dda8d7010bc654167d1b329d06
Łukasz <ntnrmedia@gmail.com>
2025-02-13 17:34:34 +0100

    opinie

--- js/strona-gl.js
+++ js/strona-gl.js
@@ -120,3 +120,84 @@ const toggleAtrakcje = (id) => {
         }, 200); 
     }
 };
+
+
+const deleteErrorMessage = () => {
+    const errorDiv = document.getElementById("errors-div")
+
+    errorDiv.classList.add("hidden")
+    errorDiv.innerHTML = ""
+}
+
+const sendEmail = async (event) => {
+    event.preventDefault()
+    
+    const email = document.getElementById("email").value
+    const firstName = document.getElementById("name").value
+    const lastName = document.getElementById("last-name").value
+    const message = document.getElementById("message").value
+    
+    const formData = JSON.stringify({email, firstName, lastName, message})
+    try {
+       const res = await fetch("https://europe-central2-light-scarab-439115-h3.cloudfunctions.net/function-1", {
+            body: formData,
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+        const data = await res.json()
+
+        console.log(data)
+
+        const errorDiv = document.getElementById("errors-div")
+        const { stan, mess } = data
+     
+        errorDiv.classList.remove("hidden")
+
+        errorDiv.innerHTML = `<div class="p-4 w-full justify-between items-center flex shadow-lg ${stan ? "bg-green-600" : "bg-red-600"} rounded-md text-white text-lg">${mess}<div onclick="deleteErrorMessage()" class=""><svg class="w-4 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></div></div>`
+
+    } catch (error) {
+        console.log(error)
+        errorDiv.classList.remove("hidden")
+
+        errorDiv.innerHTML = `<div class="p-4 w-full justify-between items-center flex shadow-lg bg-red-600 rounded-md text-white text-lg">Error<div onclick="deleteErrorMessage()" class=""><svg class="w-4 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></div></div>`
+
+    }
+
+    
+        
+}
+
+
+
+const reviewButtons = Array.from({length:3}, (_, i) => document.getElementById("review-btn-" + (i + 1)))
+
+const reviewDivs = Array.from({length:3}, (_, i) => document.getElementById("review-div-" + (i + 1)))
+
+
+const changeReviewDiv = (id) => {
+    const button = reviewButtons[id]
+    const div = reviewDivs[id]
+
+    reviewButtons.forEach(el => {
+        el.classList.replace("border-gray-800", "border-transparent")
+
+    })
+    reviewDivs.forEach(el => {
+        el.classList.replace("flex", "hidden")
+    })
+
+    if (button.classList.contains("border-transparent")) {
+        button.classList.replace("border-transparent", "border-gray-800")
+        div.classList.replace("hidden", "flex")
+    } else {
+        button.classList.replace("border-gray-800", "border-transparent")
+        div.classList.replace("flex", "hidden")
+    }
+
+}
+
+reviewButtons.forEach((el, i) => el.addEventListener("click", () => {
+    changeReviewDiv(i)
+}))
