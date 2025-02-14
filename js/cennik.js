d4d62057fbe6fc2dfee161fe6056082944ca7840
Łukasz <ntnrmedia@gmail.com>
2025-02-14 18:51:57 +0100

    borders

--- js/cennik.js
+++ js/cennik.js
@@ -5,18 +5,20 @@ const deleteErrorMessage = () => {
     errorDiv.innerHTML = ""
 }
 
-const sendEmail = async (event) => {
+const sendAskEmail = async (event) => {
     event.preventDefault()
     
-    const email = document.getElementById("email").value
-    const firstName = document.getElementById("name").value
-    const lastName = document.getElementById("last-name").value
-    const message = document.getElementById("message").value
+    const dateFrom = document.getElementById("date2").value
+    const dateTo = document.getElementById("date-to2").value
+    const adults = document.getElementById("adults-input").value
+    const kids = document.getElementById("kids-input").value
+    const kidsAge = document.getElementById("kids-age-input").value
+    const email = document.getElementById("email2").value
     
-    const formData = JSON.stringify({email, firstName, lastName, message})
+    const formData = JSON.stringify({email, dateFrom, dateTo, adults, kids, kidsAge})
     try {
       
-        const res = await fetch("https://europe-central2-light-scarab-439115-h3.cloudfunctions.net/function-1", {
+        const res = await fetch("https://europe-central2-light-scarab-439115-h3.cloudfunctions.net/function-2", {
             body: formData,
             method: "POST",
             headers: {
@@ -41,42 +43,7 @@ const sendEmail = async (event) => {
         errorDiv.innerHTML = `<div class="p-4 w-full justify-between items-center flex shadow-lg bg-red-600 rounded-md text-white text-lg">Error<div onclick="deleteErrorMessage()" class=""><svg class="w-4 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg></div></div>`
 
     }
-
     
         
 }
 
-
-const copyBtn1 = document.getElementById("copy-btn-1")
-const copyBtn2 = document.getElementById("copy-btn-2")
-
-
-const copyEl = (id) => {
-    const array = [
-        "54 1090 2835 0000 0001 5320 1446",
-        "PL54109028350000000153201446"
-    ]
-
-    navigator.clipboard.writeText(array[id])
-
-    if (id == 1) {
-        copyBtn2.textContent = "Skopiowano!"
-
-        setTimeout(() => {
-            copyBtn2.textContent = "Kopiuj"
-        }, 2000)
-    } else {
-        copyBtn1.textContent = "Skopiowano!"
-
-        setTimeout(() => {
-            copyBtn1.textContent = "Kopiuj"
-        }, 2000)
-    }
-}
-
-copyBtn1.addEventListener("click", () => {
-    copyEl(0)
-})
-copyBtn2.addEventListener("click", () => {
-    copyEl(1)
-})
