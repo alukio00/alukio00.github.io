d86e77fdac9b7de596e4730e552e13cf663d2a09
Łukasz <ntnrmedia@gmail.com>
2025-04-08 18:44:35 +0200

    fix layout

--- js/img-zoom-popup.js
+++ js/img-zoom-popup.js
@@ -1,7 +1,7 @@
 class PopupImageZoom extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <div id="popup-div" class="hidden fixed left-0 w-full h-full flex justify-center items-center flex-row z-30 bg-black bg-opacity-60">
+        <div id="popup-div" class="hidden fixed left-0 top-0 w-full h-full flex justify-center items-center flex-row z-30 bg-black bg-opacity-60">
           
         <button onclick="zoomLeft()" type="button" class="border-2 border-gray-800 hover:bg-gray-400 max-lg:hover:bg-white max-lg:bg-opacity-50 duration-200 absolute left-2  flex p-2 cursor-pointer bg-white rounded-full justify-center items-center">
             <svg class="w-10 h-10 fill-gray-800 max-md:w-7 max-md:h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/></svg>
