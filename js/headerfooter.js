d9f74fdc6e96037bf255d8f1d249250306b79dba
Łukasz <ntnrmedia@gmail.com>
2025-04-13 13:18:36 +0200

    img-wtep i a1 wyglad

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -1,7 +1,7 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = /*html*/`
-            <header id="header" class="z-10 h-[75px] w-full top-0 left-0 flex absolute justify-center items-center bg-gray-800 max-lg:hidden shadow-div duration-200">
+            <header id="header" class="z-10 h-[75px] w-full top-0 left-0 flex absolute justify-center items-center bg-gray-800 bg-opacity-10 max-lg:hidden duration-200 shadow-div">
                 <nav class="text-white flex flex-row justify-center items-center">
                     <a class="header-link" href="/">Strona Główna</a>
                     <div class="header-link header-apartamenty-parent relative">Nasza Oferta
@@ -33,7 +33,7 @@ class Header extends HTMLElement {
 
             <header class="hidden z-10 fixed top-0 left-0 h-full max-lg:flex text-white flex-row justify-start">
                 <div class="w-full">
-                    <i id="menu-icon" class="justify-center items-center fixed top-3 left-3 z-20 p-2 rounded-lg border flex bg-gray-800">
+                    <i id="menu-icon" class="justify-center items-center fixed top-3 left-3 z-20 p-2 shadow-div rounded-lg flex bg-gray-800">
                         <svg id="menu-icon-show" class="fill-white w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
                         <svg id="menu-icon-close" class="hidden fill-white w-7 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
                         </i>
